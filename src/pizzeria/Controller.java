49369a45edd5ef834bb7c588c2fd9fd1e50a7abd
Chalach <alex.programmieren@gmail.com>
2016-12-13 18:07:49 +0100

    Serialize + Mitglieder

--- src/pizzeria/Controller.java
+++ src/pizzeria/Controller.java
@@ -11,6 +11,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
+import javax.swing.*;
 import java.io.*;
 import java.net.URL;
 import java.util.*;
@@ -20,8 +21,11 @@ public class Controller extends Application implements Initializable, Serializab
     @FXML private Pizzeria myRestaurante;
     @FXML private Kundenstamm kundenstamm;
     @FXML private ArrayList<Mensch> mitarbeiter = new ArrayList<>();
-    private ArrayList<String> errorList = new ArrayList<>();
+    @FXML private static ArrayList<Pizza> pizzen = new ArrayList<>();
+    @FXML private static ArrayList<String> belag = new ArrayList<>();
+    @FXML private ArrayList<String> namenListe = new ArrayList<>();
     @FXML private static Stage mainStage;
+    @FXML private static Stage mitArbeiterStage;
     @FXML private TextField namePizzeria;
     @FXML private TextField nameInhaber;
     @FXML private TextField nachnameInhaber;
@@ -31,16 +35,66 @@ public class Controller extends Application implements Initializable, Serializab
     @FXML private CheckBox ofenErstellen;
     @FXML private TextField anzahlKellner;
     @FXML private CheckBox pizzaioloSelected;
-    @FXML private Button einstellenMitarbeiter;
-    @FXML private ImageView picture1;
+    @FXML private ImageView picture1 = new ImageView();
     @FXML private ImageView picture2;
 
+     /*
+        TODO
+        Ablauf:
+            - pizzeria kann vom Inhaber erstellt werden -> OK
+            - Zu Karrierebeginn muss er Mitarbeiter einstellen -> OK
+            - Danach entschließt er sich die pizzeria zu öffnen:
+                - evtl. Auswahl, welche Pizzen er verkaufen möchte?
+            - Simulation beginnt:
+                - Kunden kommen und gehen nun zur pizzeria
+                    - werden vom Kellner/in bediehnt
+                    - Bargeld wird vom Kellner/in und Inhaber verwaltet
+                - Inhaber kann neue Mitarbeiter einstellen
+                - Inhaber kann Mitarbeiter feuern
+                - Inhaber kann die Gerätschaften (Ofen) upgraden lassen
+    */
+
+
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        //System.out.println("Programm has been started");
+        //System.out.println("Window opened");
     }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // TODO: Bilder einfügen
+
+        // Serialize, um die schon vorhanden Pizzas ins Programm zu laden
+        readIn();
+        for (Pizza i : pizzen) {
+            System.out.println(i);
+        }
+
+        // Start - Fenster
+        Parent root = FXMLLoader.load(getClass().getResource("CreatePizzeria.fxml"));
+        primaryStage.getIcons().add(new Image("file:icons\\Pizzeria.png"));
+        primaryStage.setTitle("Erstelle deine eigene Pizzeria");
+        primaryStage.setScene(new Scene(root, 500, 340));
+
+        // Bild einfügen
+        try{
+            Image image = new Image("file:icons\\Pizzeria.png");
+            picture1.setImage(image);
+            //picture2.setImage(image);
+        }catch (Exception ex){
+            System.err.println("error");
+        }
+
+        // Fenster instanzieren, um es zu einem später Zeitpunkt zu schließen
+        mainStage = primaryStage;
+        primaryStage.show();
+    }
+
+    // Für die ganzen Fehler, die der User machen kann, zuständig
     private void errorDialog(String msg){
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Information Dialog");
@@ -48,6 +102,25 @@ public class Controller extends Application implements Initializable, Serializab
         alert.setContentText(msg);
         alert.showAndWait();
     }
+
+    // Liest Namen aus einer Textdatei aus
+    private void generateNamenListe() throws IOException{
+        try (BufferedReader br = new BufferedReader(new FileReader("namen\\namen.txt"))) {
+            for (String c; (c = br.readLine()) != null; ) {
+                namenListe.add(c);
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Konnte die Datei nicht finden!");
+        }
+    }
+
+
+    /*
+        <---------->
+          CONTROLS
+        <---------->
+     */
+
 
     @FXML
     public void createPizzeria() throws IOException {
@@ -93,11 +166,12 @@ public class Controller extends Application implements Initializable, Serializab
 
         if(kundenStammErstellen.isSelected()){
             createKundenstamm();
-            showKundenstamm();
+            //showKundenstamm();
         }
         if(mitArbeiterEinstellen.isSelected()){
             mainStage.close();
             Stage mitarbeiterStage = new Stage();
+            mitArbeiterStage = mitarbeiterStage;
             Parent root = FXMLLoader.load(getClass().getResource("CreateMitarbeiter.fxml"));
             mitarbeiterStage.getIcons().add(new Image("file:icons\\pizzeria.png"));
             mitarbeiterStage.setTitle("Mitarbeiter");
@@ -141,16 +215,18 @@ public class Controller extends Application implements Initializable, Serializab
 
     @FXML
     public void mitarbeiterEinstellen() throws IOException {
-        Random randomGenerator = new Random();
-        int input = Integer.parseInt(anzahlKellner.getText());
-        ArrayList<String> namenListe = kundenstamm.getNamenListe();
-        showKundenstamm();
         /*
             TODO: Fehlerbehegung, sollte der User falsche Daten eingeben
              - Namen der Angestellten muss noch random erstellt werden -> OK
              - Bei jedem neu angestellten Mitarbeiter wird ein gewisser Betrag abgezogen -> OK
              - Überprüfung, wenn der Kontostand leer ist. Danach kann kein neuer Mitarbeiter eingestellt werden
         */
+
+        int input = Integer.parseInt(anzahlKellner.getText());
+        System.out.println("Kellner: " + input);
+        System.out.println("Pizzaiolo: " + 1);
+        Random randomGenerator = new Random();
+        generateNamenListe();
 
         for (int i = 0; i < input; i++) {
             Kellner kellner = new Kellner(namenListe.get(randomGenerator.nextInt(70)));
@@ -163,8 +239,8 @@ public class Controller extends Application implements Initializable, Serializab
         }
         else{
             errorDialog("Sind sie sicher, dass sie keinen Pizzaiolo angestellt haben wollen?");
-            // TODO: FehFehlerbehegung, sollte der User falsche Daten eingeben
         }
+        mitArbeiterStage.close();
     }
 
     @FXML
@@ -176,32 +252,18 @@ public class Controller extends Application implements Initializable, Serializab
     public ArrayList<Mensch> getMitarbeiter(){
         return mitarbeiter;
     }
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("CreatePizzeria.fxml"));
-//        try{
-//            Image image = new Image("file:Y:\\Informatik\\Projekt\\PizzeriaFX\\Pizzeria.png");
-//            System.err.println("no error");
-//            picture1.setImage(image);
-//            //picture2.setImage(image);
-//        }catch (Exception ex){
-//            System.err.println("error");
-//        }
-        primaryStage.getIcons().add(new Image("file:icons\\pizzeria.png"));
-        primaryStage.setTitle("Erstelle deine eigene Pizzeria");
-        primaryStage.setScene(new Scene(root, 500, 340));
-        mainStage = primaryStage;
-        primaryStage.show();
-    }
 
-    @FXML private static ArrayList<Pizza> pizzen = new ArrayList<>();
-    @FXML private static ArrayList<String> belag = new ArrayList<>();
+    /*
+        <----------->
+          Serialize
+        <----------->
+     */
 
     private static void readIn() throws Exception {
         ObjectInputStream ois = new ObjectInputStream(new FileInputStream("pizzen\\pizzen.ser"));
         // TODO: While-Schleife funktioniert nicht
             // while(ois.readObject() != null) ?
-        for (int i = 0; i < pizzen.size(); i++) {
+        for (int i = 0; i < 5; i++) {
             Pizza pizza = (Pizza) ois.readObject();
             pizzen.add(pizza);
         }
@@ -209,47 +271,41 @@ public class Controller extends Application implements Initializable, Serializab
 
     @FXML
     public static void writeOut() throws Exception {
-        //addPizza();
+        addPizza();
         ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("pizzen\\pizzen.ser"));
         for (Pizza i : pizzen) {
             oos.writeObject(i);
         }
-        ArrayList<String> i = new ArrayList<>();
-        i.add("peter");
-        i.add("petwerer");
-        i.add("petwewqer");
-        Pizza pizza = new Pizza("Marina", i);
-        oos.writeObject(pizza);
-        System.out.println(pizza);
         oos.close();
     }
 
+    private static void addPizza() throws IOException, ClassNotFoundException {
+        String o;
+        String p;
+        boolean test = true;
+        boolean test2 = true;
 
-    public static void main(String[] args) throws Exception {
-//        Pizzen pizzen = new Pizzen();
-//        pizzen.writePizza();
-//        pizzen.readPizzas();
-        writeOut();
-        readIn();
-        for (Pizza i : pizzen) {
-            System.out.println(i);
+        while(test2){
+            o = JOptionPane.showInputDialog("Name Pizza:");
+            if(o.equals("exit")){
+                test2 = false;
+            }
+            while (test){
+                p = JOptionPane.showInputDialog("Belager Pizza:");
+                if (p.equals("exit")){
+                    test = false;
+                }
+                else{
+                    belag.add(p);
+                }
+            }
+            Pizza pizza = new Pizza(o, belag);
+            if(!o.equals("exit")){
+                pizzen.add(pizza);
+            }
+            System.out.println(pizza);
+            belag = new ArrayList<>();
+            test = true;
         }
-        launch(args);
-        /*
-            TODO
-            Ablauf:
-                - pizzeria kann vom Inhaber erstellt werden -> OK
-                - Zu Karrierebeginn muss er Mitarbeiter einstellen -> OK
-                - Danach entschließt er sich die pizzeria zu öffnen:
-                    - evtl. Auswahl, welche Pizzen er verkaufen möchte?
-                - Simulation beginnt:
-                    - Kunden kommen und gehen nun zur pizzeria
-                        - werden vom Kellner/in bediehnt
-                        - Bargeld wird vom Kellner/in und Inhaber verwaltet
-                    - Inhaber kann neue Mitarbeiter einstellen
-                    - Inhaber kann Mitarbeiter feuern
-                    - Inhaber kann die Gerätschaften (Ofen) upgraden lassen
-        */
     }
-
 }
