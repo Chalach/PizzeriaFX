3035f3a26c09117119dde5aa2c9ff28c018c860d
Chalach <alex.programmieren@gmail.com>
2016-12-14 20:35:57 +0100

    Cleaned

--- src/pizzeria/Pizzaiolo.java
+++ src/pizzeria/Pizzaiolo.java
@@ -2,6 +2,7 @@ package pizzeria;
 
 public class Pizzaiolo extends Mensch{
 
+    // Der Pizzaiolo sollte später ein Interface implementieren, welches ihm erlaubt Pizzen zu erstellen
     public Pizzaiolo(String name) {
         super(name);
     }
